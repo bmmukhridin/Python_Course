f6075fef6ccfeefd68c6458bc9eb5d55dce6a050
bmmukhridin <bmmukhridin@icloud.com>
2023-07-17 11:49:03 -0700

    changed color of bullet

--- Python_Game/settings.py
+++ Python_Game/settings.py
@@ -16,5 +16,5 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60,60,60)
+        self.bullet_color = (39,145,50)
         self.bullets_allowed = 3
